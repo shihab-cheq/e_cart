81a65d38d4181b80f2cc065dda21c2600b8782c9
Shihab Uddin <s.uddin@cheq.io>
2021-11-13 22:37:45 +0600

    Cart Item dismissable now

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,4 +1,6 @@
+import 'package:e_cart/providers/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartItemWidget extends StatelessWidget {
   late String id;
@@ -11,17 +13,33 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Text('\$'),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(id);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: const CircleAvatar(
+              child: Text('\$'),
+            ),
+            title: Text(title),
+            subtitle: Text("Unit Price: \$$price Quantity: $quantity"),
+            trailing: Text("${(price * quantity)}"),
           ),
-          title: Text(title),
-          subtitle: Text("Unit Price: \$$price Quantity: $quantity"),
-          trailing: Text("${(price * quantity)}"),
         ),
       ),
     );
